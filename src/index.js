adbf91e9d176eb6c8a0ef5cc230fc20886dea820
hannah <hannahjmccain@gmail.com>
2023-05-19 18:41:41 -0700

    Load App component.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App/App.tsx';
+import './index.css';
+import App from './App/App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className='bg-peach1'>
-      Hello World
-      {/* <App /> */}
+      <App />
     </div>
   </React.StrictMode>
 );
